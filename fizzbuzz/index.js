51546156517b7326ec8ffb40848e97e95de4c156
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-04-04 05:56:25 +0200

    solution of fizzBuzz

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -11,7 +11,27 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // solution 1
+  // for (let i = 1; i <= n; i++) {
+  //  if(i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+  //  else if(i % 3 === 0) console.log("fizz");
+  //  else if(i % 5 === 0) console.log("buzz");
+  //  else console.log(i);
+  // }
+  // solution 2
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    if (i % 3 === 0) str += "fizz";
+    if (i % 5 === 0) str += "buzz";
+    if (str === "") str = i;
+    console.log(str);
+  }
+}
+
+// fizzBuzz Complexity Analysis
+// Time Complexity o(n)
+
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
