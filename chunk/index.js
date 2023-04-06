f3da4972875fded97b7f7300125febdbdf0e053a
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-04-06 17:44:26 +0200

    solution of chunk

--- chunk/index.js
+++ chunk/index.js
@@ -6,7 +6,26 @@
 // chunk(["a", "b", "c", "d"], 2) --> [[ "a", "b"], ["c", "d"]]
 // chunk([0, 1, 2, 3, 4, 5], 4) -->  [[0, 1, 2, 3], [4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    const last = result[result.length - 1];
+    if (!last || last.length === size) {
+      result.push([item]);
+    } else {
+      last.push(item);
+    }
+  }
+
+  return result;
+}
+
+chunk(["a", "b", "c", "d"], 2)
+// chunk Complexity Analysis
+// Time Complexity o(n) we go every item in input Array
+// space Complexity o(n)
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -38,11 +57,17 @@ describe("Array Chunking", () => {
   it("chunk() works", () => {
     let arr = ["a", "b", "c", "d"];
     let chunked = chunk(arr, 2);
-    assert.deepEqual(chunked, [["a", "b"], ["c", "d"]]);
+    assert.deepEqual(chunked, [
+      ["a", "b"],
+      ["c", "d"],
+    ]);
 
     arr = [0, 1, 2, 3, 4, 5];
     chunked = chunk(arr, 4);
-    assert.deepEqual(chunked, [[0, 1, 2, 3], [4, 5]]);
+    assert.deepEqual(chunked, [
+      [0, 1, 2, 3],
+      [4, 5],
+    ]);
   });
 });
 
