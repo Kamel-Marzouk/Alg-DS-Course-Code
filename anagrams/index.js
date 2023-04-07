7dfa09e37c44ae9924d8fb81dfa17d6413d1b42f
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-04-07 16:38:59 +0200

    solution of anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,7 +8,45 @@
 //   anagrams('Heart!', 'EARTH') --> True
 //   anagrams('lol', 'lolc') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // solution 1
+  // //  Sanitize Input String
+  // stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
+  // stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
+  // if (stringA.length !== stringB.length) return false;
+  // const stringACharCount = {};
+  // for (let i = 0; i < stringA.length; i++) {
+  //   const aChar = stringA[i];
+  //   stringACharCount[aChar] = stringACharCount[aChar] + 1 || 1;
+  // }
+  // for (let i = 0; i < stringB.length; i++) {
+  //   const bChar = stringB[i];
+  //   if (!stringACharCount[bChar]) return false;
+  //   else stringACharCount[bChar]--;
+  // }
+  // return true;
+
+  // solution 2
+  //  Sanitize Input String
+  stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
+  stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
+
+  const  sortedStringA = stringA.split("").sort().join("");
+  const sortedStringB = stringB.split("").sort().join("");
+
+  return sortedStringA === sortedStringB;
+
+}
+
+// anagrams solution 1 Complexity Analysis
+// Time Complexity o(N + M ) we go over every character in both input String
+// space Complexity o(1) character count object has at most 26 key-val pairs
+
+// anagrams solution 2 Complexity Analysis
+// Time Complexity o(N * LOG N ) we made use of a sorting algorithm
+// space Complexity o(N) sorted strings has as long as input strings
+
+// The first solution is better than the second solution
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
