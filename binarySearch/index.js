4c01c9a95695ea79171204a3c4d096bf719f40a0
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-04-17 18:08:25 +0200

    solution of binarySearch

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -2,7 +2,18 @@
 // for the provided value.
 // The Binary Search Algorithm will return the index of value found, or -1 if not found.
 
-const binarySearch = (sortedArr, value) => {};
+const binarySearch = (sortedArr, value) => {
+  let left = 0;
+  let right = sortedArr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = sortedArr[mid];
+    if (midValue === value) return mid;
+    else if (midValue < value) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+};
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
